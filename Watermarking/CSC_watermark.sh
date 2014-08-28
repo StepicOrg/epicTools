91b180c2c4058d5b312c4cb5a64cceea47a01a8a
mehanig <mehanig@gmail.com>
2014-08-28 17:15:14 +0400

    Added shell script for watermarking and compression

---
+++ Watermarking/CSC_watermark.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo wtf
+for f in *.mp4 ; 
+do ffmpeg -i $f -i watermarklogo.png -filter_complex "overlay=(main_w-overlay_w-10):(main_h-overlay_h-10)" -codec:a copy ${f%.mp4}wtrmrk.mp4; done
